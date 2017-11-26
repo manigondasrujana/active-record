764a1eccdadd8bcc3fda470e45839d7981dd73d2
Srujana Manigonda <sm2555@afsconnect1.njit.edu>
2017-11-26 13:14:13 -0500

    five

--- database.php
+++ database.php
@@ -1,24 +1,22 @@
 <?php
-define('DATABASE', 'sm2555');
-define('USERNAME', 'sm2555');
-define('PASSWORD', 'tzw8bjLL');
-define('CONNECTION', 'sql1.njit.edu');
-class dbConn{
-	protected static $db;
-	private function __construct() {
-	try{
-	self::$db = new PDO( 'mysql:host=' . CONNECTION .';dbname=' . DATABASE, USERNAME, PASSWORD );
-        self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-	}
-	catch (PDOException $e) {
-	echo "Connection Error: " . $e->getMessage();
+namespace activerecord;
+class dbconn {
+    protected static $db;
+    private function __construct() {
+        try {
+            self::$db = new \PDO('mysql:host='.CONNECTION.';dbname='.DATABASE , USERNAME, PASSWORD);
+            self::$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         }
+        catch(\PDOException $e) {
+            echo "Connection Error: " . $e->getMessage();
         }
-	public static function getConnection() {
-	if (!self::$db) {
-	new dbConn();
-	}
-	return self::$db;
-	    }
-	    }
- ?>
+    }
+    public static function getConnection() {
+        if(!self::$db)
+        {
+            new dbconn;
+        }
+        return self::$db;
+    }
+}
+?>
